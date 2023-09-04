d846fbf91c871580902b7db1e600326ad135b653
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2023-09-04 15:23:12 -0300

    Apagando comentários de código

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -4,27 +4,6 @@
     
 */
 
-// export class Negociacao {
-//     constructor(
-//         private _data: Date,
-//         public readonly quantidade: number,
-//         public readonly valor: number
-//     ) {}
-
-//     get data(): Date {
-//         const data = new Date(this._data.getTime());
-//         return data;
-//     }
-
-//     get volume(): number {
-//         return this.quantidade * this.valor;
-//     }
-
-//     get todos(): string {
-//         return `Data: ${this.data}, Quantidade: ${this.quantidade}, Valor: ${this.valor}, Volume: ${this.volume}`
-//     }
-// }
-
 export class Negociacao {
     constructor(
         private _data: Date,
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -3,23 +3,6 @@
     Classe é o exato modelo da negociação, onde seus valores apenas podem entrar, mas não sair (Não pode ser removido nem modificado)
     
 */
-// export class Negociacao {
-//     constructor(
-//         private _data: Date,
-//         public readonly quantidade: number,
-//         public readonly valor: number
-//     ) {}
-//     get data(): Date {
-//         const data = new Date(this._data.getTime());
-//         return data;
-//     }
-//     get volume(): number {
-//         return this.quantidade * this.valor;
-//     }
-//     get todos(): string {
-//         return `Data: ${this.data}, Quantidade: ${this.quantidade}, Valor: ${this.valor}, Volume: ${this.volume}`
-//     }
-// }
 export class Negociacao {
     constructor(_data, quantidade, valor) {
         this._data = _data;
