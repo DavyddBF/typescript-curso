1626e25a63b697470a8b94d86c517c5813367e6c
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2023-09-06 19:06:25 -0300

    Resolvendo bugs "null"

--- app/app.ts
+++ app/app.ts
@@ -3,9 +3,11 @@ import { NegociacaoController } from "./controllers/negociacao-controller.js";
 const controle = new NegociacaoController();
 const formulario = document.querySelector(".form");
 
-formulario.addEventListener("submit", (evento) => {
-    evento.preventDefault();
-    controle.adiciona();
-});
-
-console.log("oi");
+if(formulario) {
+    formulario.addEventListener("submit", (evento) => {
+        evento.preventDefault();
+        controle.adiciona();
+    }) 
+} else {
+    throw Error("Negociação não pôde ser inicializada devido erro no form. Possivelmente retornou 'null'");
+}
