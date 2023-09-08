03ac3b668fd01e3e38f8c7faf276d7de52103ba8
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2023-09-08 18:58:44 -0300

    Adicionando a possibilidade de parâmetros

--- app/dist/js/decorators/logTemp.js
+++ app/dist/js/decorators/logTemp.js
@@ -1,11 +1,17 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos = false) {
     return function (target, propertyKey, descriptor) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...argumentos) {
+            let divisor = 1;
+            let unidadeDeTempo = "milisegundo";
+            if (emSegundos) {
+                divisor = 1000;
+                unidadeDeTempo = "segundos";
+            }
             const t1 = performance.now();
             const retornoDoMetodo = metodoOriginal.apply(this, argumentos);
             const t2 = performance.now();
-            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / 1000}`);
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / divisor} ${unidadeDeTempo}`);
             retornoDoMetodo;
         };
         return descriptor;
--- app/src/decorators/logTemp.ts
+++ app/src/decorators/logTemp.ts
@@ -11,7 +11,7 @@ export function logarTempoDeExecucao() {
 }
 */
 
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos: boolean = false) {
     return function(
         target: any, // Se o método for estático de uma classe pode ser uma função contrutora. Se não for estático vai ser o prototype
         propertyKey: string, // Trás o nome do método
@@ -19,10 +19,16 @@ export function logarTempoDeExecucao() {
     ) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function(...argumentos: Array<any>) {
+            let divisor = 1;
+            let unidadeDeTempo = "milisegundo";
+            if(emSegundos) {
+                divisor = 1000;
+                unidadeDeTempo = "segundos";
+            }
             const t1 = performance.now();
             const retornoDoMetodo = metodoOriginal.apply(this, argumentos);
             const t2 = performance.now();
-            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / 1000}`);
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / divisor} ${unidadeDeTempo}`);
             retornoDoMetodo;
         }
 
